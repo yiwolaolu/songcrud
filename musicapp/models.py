177d5c6fd7a625443b14050995798d67084d9900
yiwolaolu <85620039+yiwolaolu@users.noreply.github.com>
2022-10-28 02:38:26 -0500

    git add .

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    age = models.IntegerField()
+    
+    
+    # def __str__ (self)
+    #    return f"{self.first_name} {self.last_name}"
+class Song(models.Model):
+    title = models.CharField(max_length=50)    
+    date_released =models.DateTimeField(default=datetime.today)
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    
+    
+    # def __str__ (self):
+    #    return f"{self.title}"
+    
+    
+class Lyrics(models.Model):
+    content = models.TextField(max_length=1000)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)    
+
+
+    
